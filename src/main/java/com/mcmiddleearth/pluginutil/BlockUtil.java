4d1e9e445e583323edf397cde0dc9bf8960ae224
EriolEandur <eriol_eandur@gmx.de>
2020-04-18 14:05:34 +0200

    removed unused import

--- src/main/java/com/mcmiddleearth/pluginutil/BlockUtil.java
+++ src/main/java/com/mcmiddleearth/pluginutil/BlockUtil.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.DyeColor;
 import org.bukkit.Location;
